277ee179c365044a11a2190bf132817e986c56b2
Mae Mabanta <gmmabanta@gmail.com>
2021-03-01 18:53:23 +0800

    Login and Signup screens without authentication

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'mainscreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -22,29 +23,280 @@ class MyApp extends StatelessWidget {
         accentColor: Colors.grey[200],
       ),
     //  home: MainScreen(),
-      home: AuthenticationWrapper(),
+      home: LoginScreen(),
 
     );
   }
 }
 
-class AuthenticationWrapper extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Color(0xFFFF1EEEE),
+      body: Container(
+
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text("Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.teal[300],
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.all(30),
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Sign in",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        controller: nameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Username',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        GestureDetector(
+                          child: Container(
+                              padding: EdgeInsets.all(10),
+                              width: 80,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(Radius.circular(30)),
+                                border: Border.all(color: Colors.teal[300])
+
+                              ),
+                              child: Text("Sign up",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.teal[300]
+                                ),
+                              )
+                          ),
+                          onTap: () {
+                            Navigator.push(context,
+                              MaterialPageRoute(builder: (context)=>SignUpScreen()),
+                            );
+                          },
+                        ),
+
+                        GestureDetector(
+                          child: Container(
+                              padding: EdgeInsets.all(10),
+                              width: 80,
+                              height: 40,
+                              decoration: BoxDecoration(
+                                color: Colors.teal[300],
+                                borderRadius: BorderRadius.all(Radius.circular(30)),
+                              ),
+                              child: Text("Sign in",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white
+                                ),
+                              )
+                          ),
+                          onTap: () {
+                            Navigator.push(context,
+                              MaterialPageRoute(builder: (context)=>MainScreen()),
+                            );
+                          },
+                        ),
+
+                      ],
+
+                    ),
+
+                  ],
+                )
+
+            ),
+
+
+          ],
+
+        ),
+
+
+
+      )
+
+    );
   }
 }
 
-class RegisterEmailSection extends StatefulWidget {
-  final String title = 'Registration';
-
+class SignUpScreen extends StatefulWidget {
   @override
-  _RegisterEmailSectionState createState() => _RegisterEmailSectionState();
+  _SignUpScreenState createState() => _SignUpScreenState();
 }
 
-class _RegisterEmailSectionState extends State<RegisterEmailSection> {
+class _SignUpScreenState extends State<SignUpScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Color(0xFFFF1EEEE),
+        body: Container(
+
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("Sign Up",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.teal[300],
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.all(30),
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("Sign in",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Username',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: TextField(
+                          controller: passwordController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          GestureDetector(
+                            child: Container(
+                                padding: EdgeInsets.all(10),
+                                width: 80,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                                    border: Border.all(color: Colors.teal[300])
+
+                                ),
+                                child: Text("Login",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.teal[300]
+                                  ),
+                                )
+                            ),
+                            onTap: () {
+                              Navigator.push(context,
+                                MaterialPageRoute(builder: (context)=>MainScreen()),
+                              );
+                            },
+                          ),
+
+                          GestureDetector(
+                            child: Container(
+                                padding: EdgeInsets.all(10),
+                                width: 80,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: Colors.teal[300],
+                                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                                ),
+                                child: Text("Sign in",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white
+                                  ),
+                                )
+                            ),
+                            onTap: () {
+                              Navigator.push(context,
+                                MaterialPageRoute(builder: (context)=>LoginScreen()),
+                              );
+                            },
+                          ),
+
+                        ],
+
+                      ),
+
+                    ],
+                  )
+
+              ),
+
+
+            ],
+
+          ),
+
+
+
+        )
+
+    );
   }
 }
